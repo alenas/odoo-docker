a7bb353d8d949e986972cf73bbbb18960d2d57af
Alenas <alenas@hotmail.com>
2021-03-01 16:26:18 +0200

    added proxy

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ if [ -f set-env-pwd.sh ]; then
 fi
 
 mkdir -p /$podname/data
-chmod 666 /$podname/data
+chmod 777 /$podname/data
 mkdir -p /$podname/config
-chmod 666 /$podname/config
-mkdir -p /$podname/addons
-chmod 666 /$podname/addons
+chmod 777 /$podname/config
+mkdir -p /$podname/proxy
+chmod 666 /$podname/proxy
 
 mkdir -p /$podname/db
 
@@ -25,15 +25,22 @@ else
 fi
 
 if [ ! -f /$podname/config/odoo.conf ]; then
-    echo 'Copying config'
+    echo 'Copying odoo config...'
     cp ./14.0/odoo.conf /$podname/config/
     chmod 666 /$podname/config/odoo.conf
 fi
 
+if [ ! -f /$podname/proxy/traefik.yaml ]; then
+    echo 'Copying proxy config...'
+    cp ./etc/traefik/* /$podname/proxy/
+    chmod 666 /$podname/proxy/*
+fi
+
+
 echo 'Creating pod: ' $podname
 ### create pod
 podman pod create -n $podname --hostname www.pir.lt \
-    -p 80:8069/tcp -p 443:8071/tcp
+    -p 80:80/tcp -p 443:443/tcp -p 8080:8080/tcp
 
 echo 'Starting pod: ' $podname
 podman pod start $podname
@@ -47,13 +54,17 @@ podman run -d --name $podname-db --pod $podname \
     -e POSTGRES_DB=postgres \
         docker.io/library/postgres:13-alpine
 
+echo 'Running traefik reverse-proxy...'
+podman run -d --name reverse-proxy --pod $podname \
+    -v /$podname/proxy:/etc/traefik \
+        docker.io/library/traefik:2.4
+
 echo 'Running APP container: ' $podname-app
 ### create app container - run and then attach to
 podman run -d --name $podname-app --pod $podname \
     -e HOST=127.0.0.1 \
     -e USER=odoo \
     -e PASSWORD=$MYSQLPWD \
-    -v /$podname/addons:/mnt/extra-addons \
     -v /$podname/data:/var/lib/odoo \
     -v /$podname/config:/etc/odoo \
         localhost/al3nas/odoo:14.0
