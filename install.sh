94b3dc7befa53779dd58af914ca4e1e8271250ac
Alenas <alenas@hotmail.com>
2021-03-02 19:06:09 +0200

    working with crun

--- install.sh
+++ install.sh
@@ -38,7 +38,8 @@ fi
 
 echo 'Creating pod: ' $podname
 ### create pod
-podman pod create -n $podname --hostname www.pir.lt \
+podman pod create -n $podname --network=static --hostname www.pir.lt \
+    --runtime=/usr/lib/cri-o-runc/sbin/runc \
     -p 80:80/tcp -p 443:443/tcp -p 8080:8080/tcp
 
 echo 'Starting pod: ' $podname
@@ -47,6 +48,7 @@ podman pod start $podname
 echo 'Running DB container: ' $podname-db
 ### create db container
 podman run -d --name $podname-db --pod $podname \
+    --runtime=/usr/lib/cri-o-runc/sbin/runc \
     -v /$podname/db:/var/lib/postgresql/data \
     -e POSTGRES_USER=odoo \
     -e POSTGRES_PASSWORD=$MYSQLPWD \
@@ -55,12 +57,14 @@ podman run -d --name $podname-db --pod $podname \
 
 echo 'Running traefik reverse-proxy...'
 podman run -d --name reverse-proxy --pod $podname \
+    --runtime=/usr/lib/cri-o-runc/sbin/runc \
     -v /$podname/proxy:/etc/traefik \
         docker.io/library/traefik:2.4
 
 echo 'Running APP container: ' $podname-app
 ### create app container - run and then attach to
 podman run -d --name $podname-app --pod $podname \
+    --runtime=/usr/lib/cri-o-runc/sbin/runc \
     -e HOST=127.0.0.1 \
     -e USER=odoo \
     -e PASSWORD=$MYSQLPWD \
